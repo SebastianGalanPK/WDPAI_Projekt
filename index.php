8eac07d37047c05e4c4d6fa7e1fdca191949c043
Sebastian Gałan <sebastian.galan@student.pk.edu.pl>
2022-11-18 11:46:02 +0100

    Dodanie strony logowania
    - Dodanie strony logowania (signIn.php)
    - Dodanie stylu strony logowania (signInUp.css)
    - Rozpoczęcie prac nad stroną główną
    - Podstawowa konfiguracja Docker'a, nginx, php itp.

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="Styles/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;800&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div id="container">
+        <div id="panel-left">
+            <div id="left">
+                <div id="website-logo">
+                    <a href="signIn.php"><img src = Images/logo150x29.png></a>
+                </div>
+            </div>
+            <div id="right">
+                <div id="search-bar">
+                    <span class="search">Search</span>
+                </div>
+            </div>
+        </div>
+        <div id="panel-middle">
+            <div class="header">
+                <span id="community-name">
+                    Home
+                </span>
+            </div>
+        </div>
+        <div id="panel-right">
+            <div id="sign-in-button" class="button">
+                Sign In
+            </div>
+            <div id="sign-up-button" class="button">
+                Sign Up
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ signIn.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MEEME - Sign In</title>
+
+    <link rel="stylesheet" type = "text/css" href="Styles/signInUp.css">
+
+    
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;400;800&family=Koulen&display=swap" rel="stylesheet">
+
+</head>
+<body>
+    <div class="container">
+        <div class="panel-left">
+            <div class="header">
+                <span id="welcome-to"><b>Welcome to</span> <br>
+                <span id="meeme">MEEME</b></span>
+            </div>
+            <div class="form-container">
+                <form>
+                    <span class="form-title">Sign in...</span>
+                    <input name="login" type="text" placeholder="Login">
+                    <input name="password" type="password" placeholder="Password">
+                    <span class="forgot-password link">Forgot password?</span>
+                    <div class="button-container">
+                        <button class="sign-in link">Sign In</button>
+                        <button class="sign-up link">No account? <br> Sign Up</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div class="panel-right">
+            <span id="daily-dose-of">DAILY DOSE OF</span><br>
+            <span class="meemes">MEEMES</span>
+        </div>
+    </div>
+</body>
+</html>
