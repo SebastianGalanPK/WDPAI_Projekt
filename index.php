71b69ddc2fd0bb32aa315097b82c22af62e8313a
Sebastian Gałan <sebastian.galan@student.pk.edu.pl>
2022-11-24 21:37:01 +0100

    Tworzenie strony głównej 2.0
    - Dodanie pola związanego z memem (przyciski oceniania, dodawania do ulubionych i usuwania)

--- index.php
+++ index.php
@@ -10,6 +10,9 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;800&display=swap" rel="stylesheet">
+
+    <script src="https://kit.fontawesome.com/2282473dfd.js" crossorigin="anonymous"></script>
+
 </head>
 <body>
     <div class="container">
@@ -26,6 +29,36 @@
         <div class="middle-panel">
             <div class="header">
                 <span class="community-name">Home</span>
+            </div>
+
+            <div class="meme-container">
+                <div class="meme-header">
+                    <div class="user-info">
+                        <div class="user-logo">
+                            <i class="fa-solid fa-circle-user"></i>
+                        </div>
+                        <div class="user-name">
+                            UserName224
+                        </div>
+                    </div>
+                    <div class="community-name">
+                        @Poland 
+                    </div>
+                </div>
+                <div class="meme-image">
+
+                </div>
+                <div class="meme-bottom">
+                    <div class="vote-panel">
+                        <button><i class="fa-solid fa-square-caret-up"></i></button>
+                        <button><i class="fa-solid fa-square-caret-down"></i></button>
+                        <span class="vote-amount">2521</span>
+                    </div>
+                    <div class="meme-bottom-right">
+                        <button><i class="fa-solid fa-star"></i></button>
+                        <button class="trash-bin"><i class="fa-solid fa-trash"></i></button>
+                    </div>
+                </div>
             </div>
         </div>
         <div class="right-panel">
